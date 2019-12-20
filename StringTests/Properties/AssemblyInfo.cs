acf6dc3df6aa183b0cf6ec3cb02e758ec6c6d8b0
Maria <Maria@MARIA-PC>
2019-12-20 20:27:08 +0200

    clasa

---
+++ StringTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60c37113-4c5b-434b-9eb3-0afda640c7ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringReverse;
+
+namespace StringTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private StringsReverse stu;
+
+       [TestInitialize]
+       public void Setup()
+        {
+            stu = new StringsReverse();
+        }
+       
+        [TestMethod]   
+        [DataRow("ab - cd", "dc-ba")]
+        [DataRow("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
+        [DataRow("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
+        public void Should_A_String_IsEqual(string input,string expected)
+        {
+
+
+            //Act
+            string actual = stu.ReverseString(input);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
