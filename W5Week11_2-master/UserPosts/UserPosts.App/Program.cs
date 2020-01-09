baa452e9267b645726b613d3794e8b0a30daafd1
Maria <Maria@MARIA-PC>
2020-01-09 14:58:03 +0200

    Update Tema2

---
+++ W5Week11_2-master/UserPosts/UserPosts.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using UserPosts.Data;
+using UserPosts.Domain;
+using UserPosts.Services;
+
+namespace UserPosts.App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPostRepository postRepository = new PostDataAccess();
+            IUserRepository userRepository = new UserDataAccess();
+            ICommentRepository commentRepository = new CommentDataAccess();
+
+            var service = new UserService(userRepository, postRepository,commentRepository);
+
+            var response = service.GetUserActiveRespose(2);
+        }
+    }
+}
---
+++ W5Week11_2-master/UserPosts/UserPosts.Data/CommentDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserPosts.Domain;
+using UserPosts.Services;
+
+namespace UserPosts.Data
+{
+   public class CommentDataAccess : BaseDataAccess<Comment>, ICommentRepository
+    {
+        protected override string GetFile()
+        {
+            return @"..\..\..\UserPosts.Data\Files\posts.json";
+        }
+
+        public IList<Post> GetCommentByUserId(int id)
+        {
+            var list = this.GetAll();
+            return list.Where(x => x.UserId == id).ToList();
+        }
+    }
+}
